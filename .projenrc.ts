2af7e0ec5ecc64207cf8c7cec7520eb1b7e907af
yicr <yicr@users.noreply.github.com>
2025-05-27 13:42:07 +0900

    feat!: Major version bump to 1.0.0 - Initial Stable Release

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,6 +9,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: '@gammarers/aws-log-stream-event-notifier',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers/aws-log-stream-event-notifier.git',
+  majorVersion: 1,
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
   minNodeVersion: '18.0.0',
